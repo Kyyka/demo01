e9327c7e32e384b8de67f76e3404c028925d061f
k1977 <K1977@student.jamk.fi>
2017-01-19 11:09:37 +0200

    Tehtävä09 kesken

---
+++ teht09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku;
+            do
+            {
+                Console.Write("Anna luku: ");
+                luku = int.Parse(Console.ReadLine());
+            } while (luku != 0);
+
+        }
+    }
+}
