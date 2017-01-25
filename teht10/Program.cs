4d2be7c6752fdc19bfe370242e378e48028dabfb
Kyykeli <K1977@student.jamk.fi>
2017-01-26 00:18:03 +0200

    Update Program.cs

--- teht10/Program.cs
+++ teht10/Program.cs
@@ -1,22 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace teht10
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            int[] arvot = {1, 2, 33, 44, 55, 68, 77, 96, 100};
+
+            foreach (int arvo in arvot)
+            {
+                if (arvo % 2 == 0)
+                {
+                    Console.WriteLine("HEP");
+                }
+            }
+    
+ 
         }
     }
 }
